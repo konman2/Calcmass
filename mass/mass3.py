0b251327497ba832f4b0490cfe3907ee8917176b
konman2 <manukonda11@gmail.com>
2017-03-22 19:59:02 -0700

    made code more organized

--- mass/mass3.py
+++ mass/mass3.py
@@ -2,12 +2,15 @@
 import sys
 import csv
 import os
+   
 val = ""
+multiples = {}
 try:
    val = str(sys.argv[1])
 except IndexError:
    print("Enter an argument")
    sys.exit(1)
+
 
 def add_commas(orig):
    with_Commas = ""
@@ -19,66 +22,77 @@ def add_commas(orig):
    with_Commas += orig[len(orig)-1]
    return with_Commas
 
-if val.find('-'):
-   val =  val.replace('-',',')
-if val.find(' '):
-   val = val.replace(' ',',')
-if ',' not in val:
-   val = add_commas(val)
-      
-multiples = {}
+
+# adds comma markers so rest of the functions can work
+def add_markers():
+   global val
+   if val.find('-'):
+      val =  val.replace('-',',')
+   if val.find(' '):
+      val = val.replace(' ',',')
+   if ',' not in val:
+      val = add_commas(val)
+
+# adds coefficients and symbol to the multiples dictionary
 def add(mult,build):
-    com = 0
-    for i in range(len(build)-1,-1,-1):
-        if build[i] == ',':
-            com = i
-            break
-    if com == 0:
-        symb = build[com:len(build)]
-    else:
-        symb = build[com+1:len(build)]
-    multiples[symb] = mult
+   global multiples
+   com = 0
+   for i in range(len(build)-1,-1,-1):
+      if build[i] == ',':
+         com = i
+         break
+   if com == 0:
+      symb = build[com:len(build)]
+   else:
+      symb = build[com+1:len(build)]
+   multiples[symb] = mult
 
-def findNum(i):
-    if val[i].isalpha() or val[i] == ',':
-        return val[i]
-    comma = val.find(",",i)
-    if comma != -1:
-        num = val[i:comma]
-    else:
-        num = val[i:len(val)]
-    return num
-def run():
-    lastCom = 0
-    build =""
-    i = 0
-    while i < len(val):
-        if val[i] == ',':
-            lastCom = i
-            build+=val[i]
-        elif not val[i].isalpha():
-            num = int(findNum(i))
-            add(num,build)
-            
-        else:
-            build+=val[i]
-        i+=len(findNum(i))
-    return build
+# returns the complete coeficcient given the starting number
+def find_num(i):
+   global val
+   if val[i].isalpha() or val[i] == ',':
+      return val[i]
+   comma = val.find(",",i)
+   if comma != -1:
+      num = val[i:comma]
+   else:
+      num = val[i:len(val)]
+   return num
 
-def findRow(read,val):
+# returns a string with no coefficients and builds the multiples
+# dictionary
+def strip_coeff():
+   global val
+   lastCom = 0
+   build = ""
+   i = 0
+   while i < len(val):
+      if val[i] == ',':
+         lastCom = i
+         build+=val[i]
+      elif not val[i].isalpha():
+         num = int(find_num(i))
+         add(num,build)
+      else:
+         build+=val[i]
+      i+=len(find_num(i))
+   return build
+
+def find_row_element(read,val):
    for i in read:
       if i[1].strip() == val:
          return i
    return -1
-def calcSingle(val):
+
+
+def calc_single_element(val):
    count = 0
    os.chdir(os.path.dirname(os.path.abspath(__file__)))
    with open('pt-data1.csv','r') as csvfile:
       read = csv.reader(csvfile)
-      row = findRow(read,val)
+      row = find_row_element(read,val)
       if row != -1:
          num = float(row[3][:len(row[3])-3].strip())
-         
          if val in multiples:
             count+=num*multiples[val]
          else:
@@ -87,21 +101,22 @@ def calcSingle(val):
          print("Not a compound")
          sys.exit(2)
    return count
-            
-elements = run()
 
-def calculate():
+
+def calculate(elements):
    build = ""
    count = 0
    for i in range(len(elements)):
       if elements[i] == ",":
-         count+=calcSingle(build)
+         count+=calc_single_element(build)
          build = ""
       elif i == len(elements)-1:
          build+=elements[i]
-         count+=calcSingle(build)
+         count+=calc_single_element(build)
       else:
          build+=elements[i]
    return count
 
-print("%0.5f" % calculate())
+add_markers()
+elements = strip_coeff()
+print("%0.5f" % calculate(elements))
