9764f55a4bc315e83f2823d036116c0f9978da25
konman2 <manukonda11@gmail.com>
2017-04-20 12:35:06 -0700

    Made code look nicer and fixed bug with numbers

--- mass/mass.py
+++ mass/mass.py
@@ -14,69 +14,73 @@ if len(args) == 1:
 
 def add_commas(orig):
    with_Commas = ""
-   j = -1
-   for i in range(len(orig)-1):
+   for i in range(len(orig) - 1):
       with_Commas += orig[i]
-      if orig[i+1].isupper():
-         with_Commas+=","      
-   with_Commas += orig[len(orig)-1]
+      if orig[i + 1].isupper():
+         with_Commas += ","
+   with_Commas += orig[len(orig) - 1]
    return with_Commas
 
 
 # adds comma markers so rest of the functions can work
 def add_markers(val):
    if val.find('-'):
-      val =  val.replace('-',',')
+      val = val.replace('-', ',')
    if val.find(' '):
-      val = val.replace(' ',',')
+      val = val.replace(' ', ',')
    if ',' not in val:
       val = add_commas(val)
    return val
 
+
 # adds coefficients and symbol to the multiples dictionary
-def add(mult,build):
+def add(mult, build):
    global multiples
    com = 0
-   for i in range(len(build)-1,-1,-1):
+   for i in range(len(build) - 1, -1, -1):
       if build[i] == ',':
          com = i
          break
    if com == 0:
       symb = build[com:len(build)]
    else:
-      symb = build[com+1:len(build)]
+      symb = build[com + 1:len(build)]
    multiples[symb] = mult
 
+
 # returns the complete coeficcient given the starting number
-def find_num(i,val):
+def find_num(i, val):
    if val[i].isalpha() or val[i] == ',':
       return val[i]
-   comma = val.find(",",i)
+   comma = val.find(",", i)
    if comma != -1:
       num = val[i:comma]
    else:
       num = val[i:len(val)]
    return num
 
+
 # returns a string with no coefficients and builds the multiples
 # dictionary
 def strip_coeff(val):
-   lastCom = 0
    build = ""
    i = 0
    while i < len(val):
       if val[i] == ',':
-         lastCom = i
-         build+=val[i]
+         build += val[i]
       elif not val[i].isalpha():
-         num = int(find_num(i,val))
-         add(num,build)
+         num = int(find_num(i, val))
+         add(num, build)
       else:
-         build+=val[i]
-      i+=len(find_num(i,val))
+         build += val[i]
+      i += len(find_num(i, val))
+   if len(build) == 0:
+      print("Not a compound")
+      sys.exit(2)
    return build
 
-def find_row_element(read,val):
+
+def find_row_element(read, val):
    for i in read:
       if i[1].strip() == val:
          return i
@@ -86,15 +90,15 @@ def find_row_element(read,val):
 def calc_single_element(val):
    count = 0
    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-   with open('pt-data1.csv','r') as csvfile:
+   with open('pt-data1.csv', 'r') as csvfile:
       read = csv.reader(csvfile)
-      row = find_row_element(read,val)
+      row = find_row_element(read, val)
       if row != -1:
-         num = float(row[3][:len(row[3])-3].strip())
+         num = float(row[3][:len(row[3]) - 3].strip())
          if val in multiples:
-            count+=num*multiples[val]
+            count += num * multiples[val]
          else:
-            count+=num
+            count += num
       else:
          print("Not a compound")
          sys.exit(2)
@@ -106,18 +110,19 @@ def calculate(elements):
    count = 0
    for i in range(len(elements)):
       if elements[i] == ",":
-         count+=calc_single_element(build)
+         count += calc_single_element(build)
          build = ""
-      elif i == len(elements)-1:
-         build+=elements[i]
-         count+=calc_single_element(build)
+      elif i == len(elements) - 1:
+         build += elements[i]
+         count += calc_single_element(build)
       else:
-         build+=elements[i]
+         build += elements[i]
    return count
 
-for i in range(1,len(args)):
+
+for i in range(1, len(args)):
    val = add_markers(args[i])
    elements = strip_coeff(val)
    if len(args) > 2:
-      print(args[i] + ':',end=' ')
+      print(args[i] + ':', end=' ')
    print("%0.5f" % calculate(elements))
