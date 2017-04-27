b2b406ebfdfced9690d68e860c52fc896f369c4e
konman2 <manukonda11@gmail.com>
2017-04-26 19:20:29 -0700

    Added argparse for command line parsing

--- calcmass/__main__.py
+++ calcmass/__main__.py
@@ -1,19 +1,26 @@
 from __future__ import print_function
 from calcmass import mass
-import sys
+import argparse
+
 
 def main():
-   args = sys.argv
-   if len(args) == 1:
-      print("Enter an argument")
-      sys.exit(1)
-   for i in range(1, len(args)):
-      val = mass.add_markers(args[i])
-      elements = mass.strip_coeff(val)
-      if len(args) > 2:
-         print(args[i] + ':', end=' ')
-      print("%0.5f" % mass.calculate(elements))
+    parser = argparse.ArgumentParser(description='Calculate molar mass of a chemical compound.', prog='calcmass')
+    parser.add_argument('compounds', metavar='C', nargs='+',
+                        help='a compound to find the mass of')
+    args = parser.parse_args()
+    for a in args.compounds:
+        val = mass.add_markers(a)
+        elements = mass.strip_coeff(val)
+        if len(elements) == 0:
+            raise argparse.ArgumentParser.error(parser, 'Not a compound')
+        if len(args.compounds) > 1:
+            print(a + ':', end=' ')
+        if type(mass.calculate(elements)) != str:
+            print("%0.5f" % mass.calculate(elements))
+        else:
+            raise argparse.ArgumentParser.error(parser, 'Not a compound: ' +
+                                                mass.calculate(elements))
 
 
 if __name__ == "__main__":
-   main()
+    main()
--- calcmass/mass.py
+++ calcmass/mass.py
@@ -1,117 +1,105 @@
 #!/usr/bin/python
 from __future__ import print_function
-import csv
-import sys
-import os
-from calcmass import get_data
-
+from pt_data import masses
 val = ""
 multiples = {}
 
 
 def add_commas(orig):
-   with_Commas = ""
-   for i in range(len(orig) - 1):
-      with_Commas += orig[i]
-      if orig[i + 1].isupper():
-         with_Commas += ","
-   with_Commas += orig[len(orig) - 1]
-   return with_Commas
+    with_Commas = ""
+    for i in range(len(orig) - 1):
+        with_Commas += orig[i]
+        if orig[i + 1].isupper():
+            with_Commas += ","
+    with_Commas += orig[-1]
+    return with_Commas
 
 
 # adds comma markers so rest of the functions can work
 def add_markers(val):
-   if val.find('-') != -1:
-      val = val.replace('-', ',')
-   if val.find(' ') != -1:
-      val = val.replace(' ', ',')
-   if ',' not in val:
-      val = add_commas(val)
-   return val
+    if val.find('-') != -1:
+        val = val.replace('-', ',')
+    if val.find(' ') != -1:
+        val = val.replace(' ', ',')
+    if ',' not in val:
+        val = add_commas(val)
+    return val
 
 
 # adds coefficients and symbol to the multiples dictionary
 def add(mult, build):
-   global multiples
-   com = 0
-   for i in range(len(build) - 1, -1, -1):
-      if build[i] == ',':
-         com = i
-         break
-   if com == 0:
-      symb = build[com:len(build)]
-   else:
-      symb = build[com + 1:len(build)]
-   multiples[symb] = mult
+    global multiples
+    com = 0
+    for i in range(len(build) - 1, -1, -1):
+        if build[i] == ',':
+            com = i
+            break
+    if com == 0:
+        symb = build[com:]
+    else:
+        symb = build[com + 1:]
+    multiples[symb] = mult
 
 
 # returns the complete coeficcient given the starting number
 def find_num(i, val):
-   if val[i].isalpha() or val[i] == ',':
-      return val[i]
-   comma = val.find(",", i)
-   if comma != -1:
-      num = val[i:comma]
-   else:
-      num = val[i:len(val)]
-   return num
+    if val[i].isalpha() or val[i] == ',':
+        return val[i]
+    comma = val.find(",", i)
+    if comma != -1:
+        num = val[i:comma]
+    else:
+        num = val[i:]
+    return num
 
 
 # returns a string with no coefficients and builds the multiples
 # dictionary
 def strip_coeff(val):
-   build = ""
-   i = 0
-   while i < len(val):
-      if val[i] == ',':
-         build += val[i]
-      elif not val[i].isalpha():
-         num = int(find_num(i, val))
-         add(num, build)
-      else:
-         build += val[i]
-      i += len(find_num(i, val))
-   if len(build) == 0:
-      print("Not a compound")
-      sys.exit(2)
-   return build
-
-
-def find_row_element(read, val):
-   for i in read:
-      if i[1].strip() == val:
-         return i
-   return -1
+    build = ""
+    i = 0
+    while i < len(val):
+        if val[i] == ',':
+            build += val[i]
+        elif not val[i].isalpha():
+            num = int(find_num(i, val))
+            add(num, build)
+        else:
+            build += val[i]
+        i += len(find_num(i, val))
+    if len(build) == 0:
+        return ""
+    return build
 
 
 def calc_single_element(val):
-   count = 0
-   #os.chdir(os.path.dirname(os.path.abspath(__file__)))
-   with open(get_data('pt-data1.csv'), 'r') as csvfile:
-      read = csv.reader(csvfile)
-      row = find_row_element(read, val)
-      if row != -1:
-         num = float(row[3][:len(row[3]) - 3].strip())
-         if val in multiples:
-            count += num * multiples[val]
-         else:
-            count += num
-      else:
-         print("Not a compound")
-         sys.exit(2)
-   return count
+    count = 0
+    num = float(masses[val][:len(masses[val]) - 3].strip())
+    if val in multiples:
+        count += num * multiples[val]
+    else:
+        count += num
+    return count
 
 
-def calculate(elements):
-   build = ""
-   count = 0
-   for i in range(len(elements)):
-      if elements[i] == ",":
-         count += calc_single_element(build)
-         build = ""
-      elif i == len(elements) - 1:
-         build += elements[i]
-         count += calc_single_element(build)
-      else:
-         build += elements[i]
-   return count
+# returns a mass if all elements are real
+# otherwise returns the name of the offending element
+def calculate(comp):
+    build = ""
+    count = 0
+    for i in range(len(comp)):
+        if comp[i] == ",":
+            if build in masses:
+                count += calc_single_element(build)
+                build = ""
+            else:
+                return build
+        elif i == len(comp) - 1:
+            build += comp[i]
+            if build in masses:
+                count += calc_single_element(build)
+            else:
+                return build
+        else:
+            build += comp[i]
+    return count
