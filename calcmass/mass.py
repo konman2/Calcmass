92049000e443a029907f2572ac8440b06977764d
konman2 <manukonda11@gmail.com>
2017-05-07 16:14:41 -0700

    Added functionality to work with parenthesis

--- calcmass/mass.py
+++ calcmass/mass.py
@@ -15,12 +15,60 @@ def add_commas(orig):
     return with_Commas
 
 
+# returns the complete coeficcient given the starting number
+def find_num(i, val):
+    if val[i].isalpha() or val[i] == ',':
+        return val[i]
+    count = i
+    while(count < len(val) and not val[count].isalpha() and val[count] != ","):
+        count += 1
+    if count >= len(val):
+        return val[i:]
+    return val[i:count]
+
+
+def distribute(val, num):
+    build = ""
+    sub = 1
+    for i in range(len(val)):
+        if not val[i].isalpha():
+            sub = int(find_num(i, val))
+            sub *= num
+            build += str(sub)
+            i += len(find_num(i, val)) - 1
+        elif i+1 >= len(val) or (val[i+1].isupper()):
+            sub = 1*num
+            build += val[i]
+            build += str(sub)
+            i += len(find_num(i, val))
+            sub = 1
+        else:
+            build += val[i]
+    return build
+
+
+def distrib_parenth(val):
+    between = ""
+    op = val.find('(')
+    num = 1
+    while(op != -1):
+        close = val.find(')', op)
+        between = val[op+1:close]
+        if close+1 < len(val) and not val[close+1].isalpha() and val[close+1] != '':
+            num = int(val[close+1])
+            val = val[:op] + distribute(between, num) + val[close+2:]
+        else:
+            val = val[:op] + distribute(between, num) + val[close+1:]
+        op = val.find('(', op+1)
+        num = 1
+    print(val)
+    return val
+
+
 # adds comma markers so rest of the functions can work
 def add_markers(val):
-    if val.find('-') != -1:
-        val = val.replace('-', ',')
-    if val.find(' ') != -1:
-        val = val.replace(' ', ',')
+    if "(" in val and ")" in val:
+        val = distrib_parenth(val)
     if ',' not in val:
         val = add_commas(val)
     return val
@@ -39,18 +87,6 @@ def add(mult, build):
     else:
         symb = build[com + 1:]
     multiples[symb] = mult
-
-
-# returns the complete coeficcient given the starting number
-def find_num(i, val):
-    if val[i].isalpha() or val[i] == ',':
-        return val[i]
-    comma = val.find(",", i)
-    if comma != -1:
-        num = val[i:comma]
-    else:
-        num = val[i:]
-    return num
 
 
 # returns a string with no coefficients and builds the multiples
