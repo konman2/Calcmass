a7f5e115ede47da6b90b3012e2fb85d369f69742
konman2 <manukonda11@gmail.com>
2017-05-07 18:59:33 -0700

    Release 1.5

--- setup.py
+++ setup.py
@@ -2,15 +2,13 @@ from setuptools import setup
 setup(
   name = 'calcmass',
   packages = ['calcmass'],
-    version = '1.0',
+  version = '1.5',
   description = 'A script to calculate the molecular mass of a chemical compound in g/mol with its chemical formula',
   author = 'Manu Kondapaneni',
   author_email = 'manukonda11@gmail.com',
   url = 'https://github.com/konman2/Mass',
-  download_url = 'https://github.com/konman2/Mass/archive/1.0.tar.gz',
+  download_url = 'https://github.com/konman2/Mass/archive/1.5.tar.gz',
   keywords = ['mass', 'chemical', 'compound','chemistry','element','calculator'],
-  include_package_data = True,
-  package_data = {'calcmass' : ['data/*.csv']},
   entry_points={
     'console_scripts': [
             'calcmass = calcmass.__main__:main'
